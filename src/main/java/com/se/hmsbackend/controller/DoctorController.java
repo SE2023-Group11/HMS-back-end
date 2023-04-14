e8023b8744c4842bca0be094932422ad4a1c34af
AlpaCa <2604307870@qq.com>
2023-04-14 15:30:00 +0800

    完成部分Service和Controller

--- src/main/java/com/se/hmsbackend/controller/DoctorController.java
+++ src/main/java/com/se/hmsbackend/controller/DoctorController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/doctor")
 public class DoctorController {
     @Autowired
     private DoctorService doctorService;
--- src/main/java/com/se/hmsbackend/controller/SectionController.java
+++ src/main/java/com/se/hmsbackend/controller/SectionController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/section")
 public class SectionController {
     @Autowired
     private SectionService sectionService;
